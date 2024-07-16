a8876f7cebbebc08da3e528958719652d500a9e8
felipe <carlosfelipemonte1997@hotmail.com>
2024-07-16 10:46:00 -0300

    add crud

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 // import { openDb } from "./configDB.js";
-import { createTable, insertPerson } from "./controller.js/person.js";
+import {
+  createTable,
+  insertPerson,
+  updatePerson,
+} from "./controller.js/person.js";
 import express from "express";
 const app = express();
 app.use(express.json());
@@ -11,10 +15,32 @@ app.get("/", (req, res) => {
 });
 
 app.post("/cadastro", (req, res) => {
-  insertPerson(req.body);
-  res.json({
-    statusCode: 200,
-  });
+  const { name, number } = req.body;
+  if (!name || !number ) {
+    res.json({
+      statusCode: 400,
+      message: "Compre ou reserve um numero, informe nome e telefone",
+    });
+  } else {
+    insertPerson(req.body);
+    res.json({
+      statusCode: 200,
+    });
+  }
+});
+
+app.put("/cadastro", (req, res) => {
+  if (req.body && !req.body.id) {
+    res.json({
+      statusCode: 400,
+      message: "Você precisa informar o ID",
+    });
+  } else {
+    updatePerson(req.body);
+    res.json({
+      statusCode: 200,
+    });
+  }
 });
 
 app.listen(3000, () => console.log("Api rondando.."));
--- src/controller.js/person.js
+++ src/controller.js/person.js
@@ -3,16 +3,35 @@ import { openDb } from "../configDB.js";
 export async function createTable() {
   openDb().then((db) => {
     db.exec(
-      "CREATE TABLE IF NOT EXISTS Person (id INTEGER PRIMARY KEY, name TEXT, number TEXT)"
+      `CREATE TABLE IF NOT EXISTS Person (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        number TEXT,
+        is_valid BOOLEAN,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+      )`
     );
   });
 }
 
 export async function insertPerson(person) {
   openDb().then((db) => {
-    db.run("INSERT INTO Person (name, number) VALUES (?,?)", [
+    db.run("INSERT INTO Person (name, number, is_valid) VALUES (?,?,?)", [
       person.name,
       person.number,
+      person.is_valid,
+    ]);
+  });
+}
+
+export async function updatePerson(person) {
+  openDb().then((db) => {
+    db.run("UPDATE Person SET name=?, number=?, is_valid=? WHERE id=?", [
+      person.name,
+      person.number,
+      person.is_valid,
+      person.id,
     ]);
   });
 }
